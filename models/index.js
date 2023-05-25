1c0049199de1ce2fa1f903e932b4cc053976f33e
Vita Fomin <vita.bukhalo@gmail.com>
2023-05-25 19:56:52 -0400

    created relationships for the models in the index.js file

---
+++ models/index.js
@@ -0,0 +1,17 @@
+const User = require("./User");
+const Itinerary = require("./Itinerary");
+const Comment = require("./Comment");
+
+Itinerary.belongsTo(User, {
+    foreignKey: "user_id",
+});
+
+Itinerary.hasMany(Comment, {
+    foreignKey: "comment_id",
+});
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id"
+});
+
+module.exports = { User, Itinerary, Comment }
