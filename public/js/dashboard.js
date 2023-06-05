1fedf63c04eceb7b9f92049445340341926e0eac
Vita Fomin <vita.bukhalo@gmail.com>
2023-06-05 19:30:57 -0400

    added js code to be able to create new itinerary

---
+++ public/js/dashboard.js
@@ -0,0 +1,23 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector("#itinerary-name").value.trim()
+    const description = document.querySelector("#itinerary-name").value.trim()
+
+    if (title && description) {
+        const response = await fetch(`/api/itineraries`, {
+            method: "POST",
+            body: JSON.stringify({ title, description }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace("/dashboard")
+        }
+        else {
+            alert("Failed to Create Itinerary")
+        }
+    }
+}
+
+document.querySelector(".button").addEventListener("click", newFormHandler);
